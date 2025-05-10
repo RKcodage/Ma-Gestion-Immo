f8a2d2ee2230630c262109e5e03bd55ce319fd2c
RayanKabra <rkabra.dev@gmail.com>
2025-05-10 19:55:13 +0200

    Test signup and login in prod

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -27,18 +27,21 @@ const useAuthStore = create((set, get) => ({
   signup: async (formData) => {
     set({ loading: true, error: null });
     try {
-      const response = await fetch("http://localhost:4000/user/signup", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          email: formData.email,
-          password: formData.password,
-          firstName: formData.profile.firstName,
-          lastName: formData.profile.lastName,
-          username: formData.profile.username,
-          phone: formData.profile.phone,
-        }),
-      });
+      const response = await fetch(
+        "https://site--ma-gestion-immo--574qbjcqcwyr.code.run/user/signup",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            email: formData.email,
+            password: formData.password,
+            firstName: formData.profile.firstName,
+            lastName: formData.profile.lastName,
+            username: formData.profile.username,
+            phone: formData.profile.phone,
+          }),
+        },
+      );
 
       if (!response.ok) {
         const errorData = await response.json();
@@ -60,11 +63,14 @@ const useAuthStore = create((set, get) => ({
   login: async (email, password) => {
     set({ loading: true, error: null });
     try {
-      const response = await fetch("http://localhost:4000/user/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password }),
-      });
+      const response = await fetch(
+        "https://site--ma-gestion-immo--574qbjcqcwyr.code.run/user/login",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email, password }),
+        },
+      );
 
       if (!response.ok) {
         const errorData = await response.json();
