09bd36e71a3249f131d1af5ec4b6985af0a9a05f
RayanKabra <rkabra.dev@gmail.com>
2025-11-26 14:38:54 +0100

    Header changed (when user is connected)

--- src/components/homepage/Header.jsx
+++ src/components/homepage/Header.jsx
@@ -22,10 +22,7 @@ const Header = ({ className = "" }) => {
             to="/dashboard"
             className="flex items-center gap-2 text-white text-lg hover:underline underline-offset-4 leading-none"
           >
-            Revenir à mon espace
-            <span className="w-6 h-6 flex items-center justify-center rounded-full border border-white">
-              <User size={14} />
-            </span>
+            Mon espace
           </Link>
         ) : (
           <Link
